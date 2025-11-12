dc6ea79787cf035d9ec1470c50f655850925712e
SEUZSH <duoanba@gmail.com>
2025-11-12 16:39:23 +0800

    v_0.12

--- Project/temp.py
+++ Project/temp.py
@@ -1,1 +1,5 @@
 
+import pandas as pd
+
+splits = {'train': 'train.jsonl', 'validation': 'val.jsonl', 'test': 'test.jsonl'}
+df = pd.read_json("hf://datasets/bentrevett/multi30k/" + splits["train"], lines=True)
