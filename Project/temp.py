cb90edcb832d775cb21fb308c3a178a225478362
SEUZSH <duoanba@gmail.com>
2025-11-16 10:42:25 +0800

    v_1.0

--- Project/temp.py
+++ Project/temp.py
@@ -1,5 +1,9 @@
+import altair
+import vega
+import jsonschema
+import pandas
 
-import pandas as pd
-
-splits = {'train': 'train.jsonl', 'validation': 'val.jsonl', 'test': 'test.jsonl'}
-df = pd.read_json("hf://datasets/bentrevett/multi30k/" + splits["train"], lines=True)
+print(f"当前 Altair 版本: {altair.__version__}")
+print(f"当前 Vega 版本: {vega.__version__}")
+print(f"当前 JSONSchema 版本: {jsonschema.__version__}")
+print(f"当前 Pandas 版本: {pandas.__version__}")
